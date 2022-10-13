ae42e4c2bc409f6776c6979a940bac6133cdcc3c
AndyDenny <tp0yah@gmail.com>
2022-10-13 16:07:15 +0300

    add libraries OrbitControls and GLTFLoaders

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded",function () {
 
    let wwidth = window.innerWidth;
@@ -20,11 +21,50 @@ document.addEventListener("DOMContentLoaded",function () {
    let light = new THREE.AmbientLight(0xffffff);
    scene.add(light);
 
+/*
    let geometry = new THREE.PlaneGeometry(300,300,12,12);
    let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe:true})
    let mesh = new THREE.Mesh(geometry,material);
 
-   scene.add(mesh);
+   scene.add(mesh);*/
+
+   
+
+   const loader = new GLTFLoader();
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+   const dracoLoader = new DRACOLoader();   
+   dracoLoader.setDecoderPath( '/libs/draco/' );
+
+   loader.setDRACOLoader( dracoLoader );
+
+   loader.load(
+      // resource URL
+      'models/frst.glb',
+      // called when the resource is loaded
+      function ( gltf ) {
+   
+         scene.add( gltf.scene );
+   
+         // gltf.animations; // Array<THREE.AnimationClip>
+         // gltf.scene; // THREE.Group
+         // gltf.scenes; // Array<THREE.Group>
+         // gltf.cameras; // Array<THREE.Camera>
+         // gltf.asset; // Object
+   
+      },
+      // called while loading is progressing
+      function ( xhr ) {
+   
+         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+   
+      },
+      // called when loading has errors
+      function ( error ) {
+   
+         console.log( 'An error happened' );
+   
+      }
+   );
 
    renderer.render(scene,camera);
 
